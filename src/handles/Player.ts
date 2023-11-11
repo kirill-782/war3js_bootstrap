ef924ad3a996fc70d5017e8bd9383bc0535b1264
Kirill <kirill-782@yandex.ru>
2023-11-11 23:40:12 +0300

    Test

--- src/handles/Player.ts
+++ src/handles/Player.ts
@@ -6,15 +6,9 @@ const GetLocalPlayer = getNativeByName<HandleHolder<"player">, []>("GetLocalPlay
 
 export interface PlayerEventMap extends HandleEventMap {}
 
-
-
-
 export interface Player {
     get handle(): HandleHolder<"player">;
 }
-
-
-
 
 export class Player<T extends PlayerEventMap = PlayerEventMap> extends Handle<T> {
     public constructor(handleHolder: HandleHolder<"player">);
