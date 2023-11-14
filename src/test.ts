795e67e7d07e6ac5b3d03134e0d28819a760e8de
Kirill <kirill-782@yandex.ru>
2023-11-14 12:58:58 +0300

    another commit

--- src/test.ts
+++ src/test.ts
@@ -4,8 +4,6 @@ import { getNativeByName } from "@war3js/unsafe";
 import { Player, Unit } from "./index.js";
 import { TriggerUnitEvent } from "./triggerEvents/unit/TriggerUnitEvent.js";
 
-const PlayerNative = getNativeByName<HandleHolder<"player">, [number]>("Player");
-
 const u = new Unit(Player.getById(0), 0, 0, 0, 0);
 
 u.addListener("damaged", () => {});
