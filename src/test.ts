498f4eead21c71d752a8a04eeabf6059f24e348f
Kirill <kirill-782@yandex.ru>
2023-11-14 13:06:28 +0300

    test

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
-import { getNativeByName } from "@war3js/unsafe";
 import { Player, Unit } from "./index.js";
 import { TriggerUnitEvent } from "./triggerEvents/unit/TriggerUnitEvent.js";
 
